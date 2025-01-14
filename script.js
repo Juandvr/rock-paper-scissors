8624c2d852a092d4c71044577e5eb68e93ef4f91
Juandvr <villabonajuan203@gmail.com>
2025-01-14 17:44:28 -0500

    add new style for the marker

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ function getComputerChoice() {
     let computerChoice;
     let randomNumber = Math.ceil(Math.random()*3);
 
-    if (randomNumber == 1) {
-        computerChoice = "Rock";
-    } else if (randomNumber == 2) {
-        computerChoice = "Paper";
-    } else if (randomNumber == 3) {
-        computerChoice = "Scissors";
-    }
+    randomNumber == 1 ? computerChoice = "Rock" : randomNumber == 2 ? computerChoice = "Paper" : computerChoice = "Scissors";
 
     return computerChoice;
 }
@@ -24,6 +18,14 @@ function playRound(playerSelection, computerSelection) {
     const log = document.getElementById("log");
     const message = document.createElement("p");
 
+    log.innerHTML = "";
+
+    const playerChoice = document.getElementById("playerChoice");
+    const cpuChoice = document.getElementById("cpuChoice");
+
+    playerSelection == "rock" ? playerChoice.innerHTML = "&#128074;" : playerSelection == "paper" ? playerChoice.innerHTML = "&#129306;" : playerChoice.innerHTML = "&#9996;"; 
+    computerSelection == "rock" ? cpuChoice.innerHTML = "&#128074;" : computerSelection == "paper" ? cpuChoice.innerHTML = "&#129306;" : cpuChoice.innerHTML = "&#9996;"; 
+
     if (playerSelection == computerSelection) {
         message.textContent = "Tie";
         log.appendChild(message);
@@ -31,33 +33,33 @@ function playRound(playerSelection, computerSelection) {
 
     if (computerSelection == "rock") {
         if (playerSelection == "paper") {
-            message.textContent = `Player +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = `Player + 1`;
             log.appendChild(message);
             playerScore++;
         } else if (playerSelection == "scissors") {
-            message.textContent = `CPU +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = `CPU + 1`;
             log.appendChild(message);
             cpuScore++;
         }
     }
     if (computerSelection == "paper") {
         if (playerSelection == "rock") {
-            message.textContent = `CPU +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = `CPU + 1`;
             log.appendChild(message);
             cpuScore++;
         } else if (playerSelection == "scissors") {
-            message.textContent = `Player +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = `Player + 1`;
             log.appendChild(message);
             playerScore++;
         }
     }
     if (computerSelection == "scissors") {
         if (playerSelection == "paper") {
-            message.textContent = ` CPU +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = ` CPU + 1`;
             log.appendChild(message);
             cpuScore++;
         } else if (playerSelection == "rock") {
-            message.textContent = `Player +1 - Player: ${playerSelection} - CPU: ${computerSelection}`;
+            message.textContent = `Player + 1`;
             log.appendChild(message);
             playerScore++;
         }
